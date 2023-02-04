e78041a1aae58ed24ace7ba5bb6c98f6807ea6bb
Viaceslavus <vaceslav.s@protonmail.com>
2023-02-04 18:23:56 +0000

    initial

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "mbed.h"
+
+
+#define BLINKING_RATE     100ms
+
+
+int main()
+{
+    // Initialise the digital pin LED1 as an output
+    DigitalOut led(LED1);
+
+    while (true) {
+        led = !led;
+        ThisThread::sleep_for(BLINKING_RATE);
+    }
+}
